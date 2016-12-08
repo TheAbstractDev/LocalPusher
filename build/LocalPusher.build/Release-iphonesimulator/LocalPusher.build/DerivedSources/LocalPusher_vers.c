60b0140427c487dc37121fe9514a6c64d01ab91c
Sofiane Beors <theabstractdev@gmail.com>
2016-12-08 13:14:45 +0100

    removed build folder

--- build/LocalPusher.build/Release-iphonesimulator/LocalPusher.build/DerivedSources/LocalPusher_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char LocalPusherVersionString[];
- extern const double LocalPusherVersionNumber;
-
- const unsigned char LocalPusherVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:LocalPusher  PROJECT:LocalPusher-1" "\n";
- const double LocalPusherVersionNumber __attribute__ ((used)) = (double)1.;
--- build/LocalPusher.build/Release-iphonesimulator/LocalPusher.build/Objects-normal/i386/LocalPusher_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/Sofiane/Desktop/LocalPusher/build/LocalPusher.build/Release-iphonesimulator/LocalPusher.build/DerivedSources/LocalPusher_vers.c
